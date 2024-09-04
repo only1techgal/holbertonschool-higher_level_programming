175056fba4c05cb2c96760bc33140bd7e7170e3b
only1techgal <jenn.lipholo@gmail.com>
2024-09-04 16:06:05 +0200

    This project dives into Python - if/else, loops, functions

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[74:83]) # 'object-oriented programming with Python'
+print(str[39:67] + str[67:82])
 print(str)
