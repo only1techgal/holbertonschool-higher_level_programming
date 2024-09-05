ea93b4a0a6b41dedf519d026e92cd608d8600429
only1techgal <jenn.lipholo@gmail.com>
2024-09-05 14:53:41 +0200

    2. I sometimes suffer from insomnia

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-# Print the ASCII alphabet, in lowercase without a new line at the end
-print(''.join(chr(97 + i) for i in range(26)), end='')
+print("".join("{:c}".format(i) for i in range(97, 123)), end='')
