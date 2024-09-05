f3300a96124a1df48876687be062f99370cee4ee
only1techgal <jenn.lipholo@gmail.com>
2024-09-05 16:27:56 +0200

    6. Inventing is a combination of brains and materials.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02}".format(i*10 + j), end=", " if not (i == 8 and j == 9) else "\n")
+        print("{:02}".format(
+            i*10 + j), end=", " if not (i == 8 and j == 9)else "\n")
