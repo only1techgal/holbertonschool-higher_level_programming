0ce842a5a3fc3b318561c3acb53c295d83282e7b
only1techgal <jenn.lipholo@gmail.com>
2024-09-05 13:02:07 +0200

    2. I sometimes suffer from insomnia

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 # Print the ASCII alphabet, in lowercase without a new line at the end
-
-# Use a single print function and a loop to print characters from 'a' to 'z'
 print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1)), end='')
