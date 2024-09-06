d978eb46841ec7e8b858d4c63b0f60ebbb93e0fc
only1techgal <jenn.lipholo@gmail.com>
2024-09-06 11:24:51 +0200

    8. To uppercase

--- python-if_else_loops_functions/main_0.py
+++ python-if_else_loops_functions/main_0.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-
 def uppercase(s):
-    if s == "holberton":
-        print("HOLBERTON")
-    else:
-        result = ""
-        for char in s:
-            if 'a' <= char <= 'z':
-                temp = chr(ord(char) - 32)
-            else:
-                temp = char
-            result += temp
-        print(result)
+    # This function will convert the input string to uppercase and print it
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            temp = chr(ord(char) - 32)
+        else:
+            temp = char
+        result += temp
+    print(result)
 
-# Test the function
+# Only print HOLBERTON if the input is 'holberton'
 uppercase("holberton")  # Expected to print: HOLBERTON
