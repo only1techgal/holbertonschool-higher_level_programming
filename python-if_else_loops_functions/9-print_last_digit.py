2ab8fca49a96935a752d516cbd8aee6cedb41773
only1techgal <jenn.lipholo@gmail.com>
2024-09-06 12:28:43 +0200

    9. There are only 3 colors

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """
+
+    Prints the entire number.
+
+    Parameters:
+    number (int): The  number to be printed
+    """
+    print(number)
+   
+# Test the function
+print_last_digit(8044) # This should print: 8044
