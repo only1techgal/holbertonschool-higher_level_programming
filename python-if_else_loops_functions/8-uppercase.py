3ee93ccb0dcb1e33ad1ae3586a90210dd2203d66
only1techgal <jenn.lipholo@gmail.com>
2024-09-06 12:00:10 +0200

    8. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
 def uppercase(s):
+
+    """
+    Converts all lowercase letters in the string s to uppercase and prints it.
+
+    Parameters:
+    s (str): The string to be converted to uppercase.
+    """
     for char in s:
         if 'a' <= char <= 'z':
             # Convert to uppercase
@@ -8,6 +16,7 @@ def uppercase(s):
             temp = char
         print("{}".format(temp), end='')
     print()  # Print a newline after the entire string has been processed
+
 
 # Test the function
 uppercase("BEST")
