366232c5dacb1c272883262a723b714fe1ea4ab1
only1techgal <jenn.lipholo@gmail.com>
2024-09-06 13:00:01 +0200

    10. a + b

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Prints the last digit number
+
+    Parametres:
+    a (int): The first integer.
+    b (int): The second integer.
+    
+    Returns:
+    int: The sum of a and be.
+    """
+    return a + be
+
+    # Test the function
+    result1 = add(3, 98)
+    result2 = add(98, 0)
+
+    print(result1) # Output should be: 101
+    print(result2) # Output should be: 98
+    
+    
