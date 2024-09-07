178287fb8d8fb1581075c5451fa10731793c8665
only1techgal <jenn.lipholo@gmail.com>
2024-09-07 12:06:49 +0200

    1. My first toolbox!

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div  # Import the calculation function
+
+    a = 10  # Assign 10 to a
+    b = 5   # Assign 5 to b
+
+# Perform poerations and print the results
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
