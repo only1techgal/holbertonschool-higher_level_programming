b1d374602c30370d556f0091db224dfaf65ae38a
only1techgal <jenn.lipholo@gmail.com>
2024-09-07 14:45:28 +0000

    4. Who are you?

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i][:2] != "__":
-            print("{:5}".format(allf[i]))
