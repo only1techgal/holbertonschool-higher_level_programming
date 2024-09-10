c462100ee953c0641a360955ff44e06a5db4ab00
only1techgal <jenn.lipholo@gmail.com>
2024-09-10 16:34:37 +0200

    1. Secure access to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     # Retrieves an element from a list
-    # idx should return none, if negative
+    # idx should return None, if negative
     if idx < 0 or idx >= len(my_list):
-        return none
+        return None
     return my_list[idx]
