39a9d4136bc3cf3eb11b77e7dc0f866a8f655786
only1techgal <jenn.lipholo@gmail.com>
2024-09-11 15:17:00 +0200

    7. Tuples addition

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    #Adds 2 tuples
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+
+    # Add the first elements and the second elements
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return result
+    
