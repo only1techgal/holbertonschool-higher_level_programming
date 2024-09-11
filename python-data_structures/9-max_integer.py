6e0a7558f8590acb0f4d0d7ed304f0650109c312
only1techgal <jenn.lipholo@gmail.com>
2024-09-11 15:55:54 +0200

    9. Find the max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,11 +3,11 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None  # Return None if the list is empty
-    
+
     max_val = my_list[0]  # Assume the first element is the largest initially
-    
+
     for num in my_list:
         if num > max_val:  # If a larger number is found, update max_val
             max_val = num
-    
+
     return max_val  # Return the largest number found
