74f58f8b0d9d3340e584ec6e1e2e6e77bda34da9
only1techgal <jenn.lipholo@gmail.com>
2024-09-12 12:29:26 +0200

    3. Present in both

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    # Returns a set of common elements in two sets
+    # Returns a list of common elements between 'list1' and 'list2'
+    common_elements = list(set(set_1) & set(set_2)) # Find insertation and convertto list
+    return common_elements
