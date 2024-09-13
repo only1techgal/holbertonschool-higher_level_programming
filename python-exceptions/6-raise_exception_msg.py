a3899f01d58bda068430395724306ff9e6157efe
only1techgal <jenn.lipholo@gmail.com>
2024-09-13 16:08:14 +0200

    6. Raise a message

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
+    # Raises a name exception with a message
     raise NameError(message)
-
-
+# Testing the function
 try:
-    raise_exception_msg("This is a custom name error message")
+    raise_exception_msg("C is fun")
 except NameError as e:
-    print(f"Caught an exception: {e}")
+    print(e)    # Print the exception message directly, no extra text
