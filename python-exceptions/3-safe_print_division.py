be4ce70077d184b19899cc2e0ee4fe6167b696e0
only1techgal <jenn.lipholo@gmail.com>
2024-09-13 13:30:18 +0200

    3. Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    # divides 2 integers and prints the result
     result = None
     try:
         result = a / b
+        print("{} / {} = {:.1f}".format(a, b, result))
     except ZeroDivisionError:
-        print("12 / 0 = Non")
+        print("{} / {} = None".format(a, b))
     finally:
         print("Inside result: {}".format(result))
         return result
