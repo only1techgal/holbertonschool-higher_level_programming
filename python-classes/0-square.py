4ddd42d3953ab63a838abb15bcee579d2921fa5b
only1techgal <jenn.lipholo@gmail.com>
2024-09-18 14:08:12 +0200

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
-#  Writes an empty class Square whcih defines a square
+"""
+Writes an empty class Square whcih defines a square
+The class Square doesn't have any attributes or methods
+""" 
 class Square:
+    """
+    This is an empty class that defines a square
+    """
     pass
