77b47da1503084a74e96c87783a7f4613fb19ae3
only1techgal <jenn.lipholo@gmail.com>
2024-09-18 14:24:20 +0200

    1. Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,7 +10,6 @@ class Square:
     A class that defines a square with a private instance attribute size
     """
 
-
     def __init__(self, size):
         """
         Initializes a new instance of the square class
