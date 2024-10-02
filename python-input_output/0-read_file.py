1e2e1747647999e3a116b46e345967426314f802
only1techgal <jenn.lipholo@gmail.com>
2024-10-02 13:49:32 +0200

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,19 @@
-#!/usr/bin/python3
+"""
+Module: read_file_module
+
+This module contains a function to read a UTF-8 text file and print its contents
+to standard output.
+"""
 
 def read_file(filename=""):
-    """Reads the text file and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end= "")
+    """Read a text file (UTF-8) and print it to stdout.
 
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")  # Use end="" to avoid adding an extra newline
+
+# Example usage
 if __name__ == "__main__":
     read_file("my_file_0.txt")
