122fcb4c2d2179f0ba7fa5e07973e9571f51d179
only1techgal <jenn.lipholo@gmail.com>
2024-10-03 12:29:56 +0200

    9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/binpython3
+
+"""
+This module defines a Student class with attributes and a method to
+retrieve a dictionary representation of the instance
+"""
+
+class Student:
+    """
+    Represents a student with first name, last name, and age
+    """
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
