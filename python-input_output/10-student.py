e220aecbf7a8bf1a9b133e8997a2b1bedc9f543c
only1techgal <jenn.lipholo@gmail.com>
2024-10-03 13:01:40 +0200

    10. Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,13 +5,14 @@ This module defines a Student class with attributes and ma method
 to retrieve a dictionary representation of the instance
 """
 
+
 class Student:
     """
     Represents a student with first name, last name, and age
     """
 
     def __init__(self, first_name, last_name, age):
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
