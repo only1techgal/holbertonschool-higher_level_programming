88d2fd4fec3d29d255295c7ab672bacefec9849d
only1techgal <jenn.lipholo@gmail.com>
2024-10-03 13:00:26 +0200

    10. Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Student class with attributes and ma method
+to retrieve a dictionary representation of the instance
+"""
+
+class Student:
+    """
+    Represents a student with first name, last name, and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if attrs is None:
+            return {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+            }
+        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
