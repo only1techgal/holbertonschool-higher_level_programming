f1c86ea2d7f78967f5dfe15fdb0bbfb12d0d5cec
only1techgal <jenn.lipholo@gmail.com>
2024-10-30 12:50:09 +0200

    9. Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- This script creates a table 'second_table' in the database 'hbtn_0c_0'
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT PRIMARY KEY,
+	name VARCHAR(256),
+	score INT
+);
+
+-- Deletes all records from the table
+DELETE FROM second_table;
+
+-- Inserts multiple rows into the second_table
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
+
