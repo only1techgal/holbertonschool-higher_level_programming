6aeccea94759b8855287b8fcd5f52ba24bd8f16e
only1techgal <jenn.lipholo@gmail.com>
2024-10-30 11:59:30 +0200

    8. Count 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- This script displays the number of records with id = 89 in the table 'first_table' of the database 'hbtn_0c_0'
-SELECT COUNT(89) AS number_of_records FROM first_table;
+SELECT COUNT(*) AS count_89 FROM first_table WHERE id = 89;
