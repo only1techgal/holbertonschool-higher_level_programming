cab47274fb980522063c93f7ff857681731a26b9
only1techgal <jenn.lipholo@gmail.com>
2024-10-30 16:39:37 +0200

    12. Cheating is bad

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
 -- This script updates the score of Bob to 10 in the table 'second_table'
-SELECT score, name
-FROM second_table
-WHERE score = 10
-ORDER BY score DESC
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
